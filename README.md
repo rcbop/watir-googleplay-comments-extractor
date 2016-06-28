a3590f374ffa8a5455082898f367e755c9e22756
Rogério Peixoto <rogerio.c.peixoto@accenture.com>
2016-06-27 21:11:32 -0300

    fixing tag a not found, adding README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # watir-googleplay-comments-extractor
-Watir Webdriver script to extract comments of given app id in google play
+Watir Webdriver script to extract comments of given app
+id in google play to csv file
+
+## usage:
+1) brew install phantomjs
+2) gem install watir-webdriver
+3) ruby comments-extractor.rb "com.android.chrome"
--- comments-extractor.rb
+++ comments-extractor.rb
@@ -1,47 +1,49 @@
 require 'watir-webdriver'
 require 'csv'
 
-# b = Watir::Browser.new :phantomjs
-b = Watir::Browser.new :firefox
+b = Watir::Browser.new :phantomjs
+# b = Watir::Browser.new :firefox
+
+app_id = ARGV[0]
 
 b.window.maximize
 begin
-  b.goto 'https://play.google.com/store/apps/details?id=com.pontomobi.farm'
+  b.goto "https://play.google.com/store/apps/details?id=#{app_id}"
   puts ">> #{b.title}"
 
   next_button = b.buttons(class: 'expand-next')[1]
   next_button.click
 
-  h1_resenha = b.div(class: 'details-section-heading').h1(class: 'heading')
-  container = h1_resenha.parent.parent
+  h1_review = b.div(class: 'details-section-heading').h1(class: 'heading')
+  container = h1_review.parent.parent
   counter = 0
-  CSV.open('extracted-reviews.csv', 'wb', { col_sep: ';' }) do |csv|
+
+  output_filename = 'extracted-reviews.csv'
+  File.delete output_filename if File.exists? output_filename
+
+  CSV.open(output_filename, 'wb', { col_sep: ';' }) do |csv|
     while next_button.visible?
-      puts '>> Parsing page'
+      puts '>> extracting comments'
 
-      reviews_dates = container.elements(class: 'review-date')
-      puts reviews_dates.size
+      reviews = container.elements(class: 'single-review').select {|div| div.visible?}
+      puts "reviews found #{reviews.size}"
 
-      reviews_dates.each do |review_date|
-        review = review_date.parent.parent.parent
-        next if review.span(:class, 'author-name').a.text.empty?
-        author = review.span(:class, 'author-name').a.text
-        date = review_date.text
-        review = review.div(:class, 'review-body').text
+      reviews.each do |rev|
+        next if rev.span(:class, 'author-name').text.empty?
+        author = rev.span(:class, 'author-name').text
+        date = rev.span(:class, 'review-date').text
+        review = rev.div(:class, 'review-body').text
         puts "#{counter+=1} #{author},#{date}, #{review}"
         csv << [author, date, review]
       end
       puts '>> Clicking next'
       next_button.click
-      sleep 10
+      sleep 2
     end
 
     puts '>> Button next not visible. End of script!'
   end
-
   b.close
 rescue Exception => e
-  puts e.backtrace
-  b.close
   puts e
 end
